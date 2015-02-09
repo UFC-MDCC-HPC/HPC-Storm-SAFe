ac1d4e46c70f93a0f0c1d26084ce5158cc023493
jeffersoncarvalho@gmail.com <jeffersoncarvalho@gmail.com@edcd30f0-1296-f4df-92bd-a93c3a19b963>
2015-02-09 17:15:30 +0000

    renaming

---
+++ safe-hpc/src/br/ufc/mdcc/pargo/safe/exception/HPCStormException.java
@@ -0,0 +1,8 @@
+package br.ufc.mdcc.pargo.safe.exception;
+
+public class HPCStormException extends Exception{
+
+	public HPCStormException() {
+		// TODO Auto-generated constructor stub
+	}
+}
--- safe-hpc/src/br/ufc/mdcc/pargo/safe/exception/HStormWorkflowCreationException.java
+++
@@ -1,8 +0,0 @@
-package br.ufc.mdcc.pargo.safe.exception;
-
-public class HStormWorkflowCreationException extends Exception{
-
-	public HStormWorkflowCreationException() {
-		// TODO Auto-generated constructor stub
-	}
-}
