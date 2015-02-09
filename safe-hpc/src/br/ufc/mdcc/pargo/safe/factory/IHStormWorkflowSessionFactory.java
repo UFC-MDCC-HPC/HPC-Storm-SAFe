9165d4127fce2b7a396a8b854d4565a99854626c
jeffersoncarvalho@gmail.com <jeffersoncarvalho@gmail.com@edcd30f0-1296-f4df-92bd-a93c3a19b963>
2015-02-09 17:12:44 +0000

    deleting

--- safe-hpc/src/br/ufc/mdcc/pargo/safe/factory/IHStormWorkflowSessionFactory.java
+++
@@ -1,8 +0,0 @@
-package br.ufc.mdcc.pargo.safe.factory;
-
-import br.ufc.mdcc.pargo.safe.base.IHStormWorkflowSession;
-
-public interface IHStormWorkflowSessionFactory {
-
-	public IHStormWorkflowSession createHStormWorkflowSession();
-}
